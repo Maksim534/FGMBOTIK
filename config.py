5cc0ab088c188dd2af93d7472320c1dba7976717
Maksim534 <129356035+Maksim534@users.noreply.github.com>
2026-01-09 02:16:40 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+API_TOKEN = '7092521991:AAG18Ty2cie-czUSvfhQmyWo9sZmIpHtYos'
+
+admin = []
+start_money = 10000
+
+bot_name = 'FGM'
+chat = 't.me/ge'
+channel = 't.me/ge'
+admin_username = '@geormor'
+bot_username = 'FGM'
+
+chat_log = 0
+cleaning = 60
+
+custom_modules = True
