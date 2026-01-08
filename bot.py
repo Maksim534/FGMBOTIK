79cc6e80f1a909f1ca40899a66f5cf1b8a3c0bf5
Maksim534 <129356035+Maksim534@users.noreply.github.com>
2026-01-09 02:13:41 +0300

    Delete bot.py

--- bot.py
+++
@@ -1,9 +0,0 @@
-# bot version: 2.0.0,2
-
-import config as cfg
-from aiogram import Bot, Dispatcher
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
-
-
-bot = Bot(cfg.API_TOKEN, parse_mode='HTML')
-dp = Dispatcher(bot, storage=MemoryStorage())
