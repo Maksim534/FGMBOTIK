704b3c28a4fd28a998fa743a06e55f407776c979
Maksim534 <129356035+Maksim534@users.noreply.github.com>
2026-01-09 14:00:56 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,8 +3,7 @@
 import config as cfg
 from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
-from aiogram.types import DefaultBotProperties
 
-bot = Bot(cfg.API_TOKEN, default=DefaultBotProperties(parse_mode='HTML', link_preview_is_disabled=True))
+bot = Bot(cfg.API_TOKEN, (parse_mode='HTML', link_preview_is_disabled=True))
 dp = Dispatcher(bot, storage=MemoryStorage())
 
