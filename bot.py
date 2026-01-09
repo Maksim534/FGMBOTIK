ae84431b1b1df20cf0814be0b1fee8989949daec
Maksim534 <129356035+Maksim534@users.noreply.github.com>
2026-01-09 14:05:46 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,9 +3,8 @@
 import config as cfg
 from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
-from telegram.ext import Updater, DefaultBotProperties
 
 
 bot = Bot(token='7092521991:AAG18Ty2cie-czUSvfhQmyWo9sZmIpHtYos', default=DefaultBotProperties(parse_mode='HTML'))
-updater = Updater(bot=bot, use_context=True)
+dp = Dispatcher(storage=MemoryStorage())
 
