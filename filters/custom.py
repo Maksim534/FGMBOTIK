a99663a17acf9eb6b168673375eb6b21b855ed2f
Maksim534 <129356035+Maksim534@users.noreply.github.com>
2026-01-09 13:47:44 +0300

    Update custom.py

--- filters/custom.py
+++ filters/custom.py
@@ -1,7 +1,22 @@
 from typing import Union
 
+from aiogram.filters import BaseFilter
 from aiogram.types import Message, CallbackQuery
 
+
+class TextIn(BaseFilter):
+    def __init__(self, *values: str) -> None:
+        self.values = [v.lower() for v in values]
+
+    async def __call__(self, obj: Union[Message, CallbackQuery]) -> bool:
+        text = None
+
+        if isinstance(obj, Message):
+            text = obj.text
+        elif isinstance(obj, CallbackQuery):
+            text = obj.data
+
+        return text and text.lower() in self.values
 
 
 class StartsWith(BaseFilter):
