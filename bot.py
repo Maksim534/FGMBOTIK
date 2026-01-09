44066e4668cda4f52f0601e697fbd1ad8adbc058
Maksim534 <129356035+Maksim534@users.noreply.github.com>
2026-01-09 13:04:40 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,5 +4,5 @@ from aiogram import Bot, Dispatcher
 import config as cfg
 
 
-bot = Bot(token=cfg.API_TOKEN, (parse_mode='html', link_preview_is_disabled=True))
+bot = Bot(token=API_TOKEN, parse_mode=ParseMode.HTML)
 dp = Dispatcher(storage=MemoryStorage())
