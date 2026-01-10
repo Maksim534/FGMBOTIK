c555a3e4051e352d25d03e9d8d53a5fa73a1caea
Maksim534 <129356035+Maksim534@users.noreply.github.com>
2026-01-10 17:49:10 +0300

    Update db.py

--- commands/entertaining/earnings/business/db.py
+++ commands/entertaining/earnings/business/db.py
@@ -4,7 +4,7 @@ from decimal import Decimal
 
 async def buy_business(user_id: int) -> None:
     balance = cursor.execute('SELECT balance FROM users WHERE user_id = ?', (user_id,)).fetchone()[0]
-    summ = Decimal(balance) - Decimal('500000000')
+    summ = Decimal(balance) - Decimal('100000')
 
     cursor.execute('INSERT INTO business (user_id, balance, nalogs, territory, bsterritory) VALUES (?, ?, ?, ?, ?)', (user_id, 0, 0, 5, 1))
     cursor.execute('UPDATE users SET balance = ? WHERE user_id = ?', (str(summ), user_id))
@@ -57,6 +57,6 @@ async def sell_business(user_id: int, ch: int) -> None:
 
 
 async def autobusiness() -> None:
-    cursor.execute('UPDATE business SET balance = balance + ROUND((90000000 * bsterritory) / 15) WHERE nalogs < 5000000')
+    cursor.execute('UPDATE business SET balance = balance + ROUND((150000 * bsterritory) / 15) WHERE nalogs < 5000000')
     cursor.execute('UPDATE business SET nalogs = nalogs + 200000 WHERE nalogs < 5000000')
     conn.commit()
