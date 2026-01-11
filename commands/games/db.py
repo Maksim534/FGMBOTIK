d1698a548f1a50ac073b6f64e5591d42f794fe0f
Maksim534 <129356035+Maksim534@users.noreply.github.com>
2026-01-11 12:15:20 +0300

    Update db.py

--- commands/games/db.py
+++ commands/games/db.py
@@ -1,6 +1,19 @@
 from decimal import Decimal
 from commands.db import conn, cursor
 
+async def update_balance(user_id: int, amount: int | str, operation="subtract") -> None:
+	balance = cursor.execute('SELECT balance FROM users WHERE user_id = ?', (user_id,)).fetchone()[0]
+	
+	if operation == 'add':
+		new_balance = Decimal(str(balance)) + Decimal(str(amount))
+	else:
+		new_balance = Decimal(str(balance)) - Decimal(str(amount))
+	
+	new_balance = "{:.0f}".format(new_balance)
+	cursor.execute('UPDATE users SET balance = ? WHERE user_id = ?', (str(new_balance), user_id))
+	cursor.execute('UPDATE users SET games = games + 1 WHERE user_id = ?', (user_id,))
+	conn.commit()
+
 
 async def upgames(user_id: int) -> None:
     cursor.execute(f'UPDATE users SET games = games + 1 WHERE user_id = ?', (user_id,))
