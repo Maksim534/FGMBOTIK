fed792c09b07e494cbc27a742ed4766db7b1c28d
Maksim534 <129356035+Maksim534@users.noreply.github.com>
2026-01-11 12:04:04 +0300

    Update rz.py

--- commands/entertaining/rz.py
+++ commands/entertaining/rz.py
@@ -2,7 +2,7 @@ import random
 import re
 
 
-from assets import keyboards as kb
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram import Dispatcher, types
 from assets.antispam import antispam
 from commands.db import get_colvo_users, setname
