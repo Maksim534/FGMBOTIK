b05f8114f44ec4b9dbeec7583e43f66a10248b99
Maksim534 <129356035+Maksim534@users.noreply.github.com>
2026-01-11 12:09:45 +0300

    Update rz.py

--- commands/entertaining/rz.py
+++ commands/entertaining/rz.py
@@ -215,7 +215,7 @@ async def oxota(message: types.Message, user: BFGuser):
 
 
 def reg(dp: Dispatcher):
-	dp.message.register(oxota, lambda message: message.text.lower().startswith('охота'))
+	dp.message.register(oxota, StartsWith('охота'))
     dp.message.register(shar_cmd, StartsWith("шар "))
     dp.message.register(vibor_cmd, StartsWith("выбери "))
     dp.message.register(shans_cmd, StartsWith("шанс "))
