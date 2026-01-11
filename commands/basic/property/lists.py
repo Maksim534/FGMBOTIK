9b7aa4c500c2e32bbfa50d381fe6e38b038b5dd1
Maksim534 <129356035+Maksim534@users.noreply.github.com>
2026-01-11 17:33:38 +0300

    Update lists.py

--- commands/basic/property/lists.py
+++ commands/basic/property/lists.py
@@ -2,31 +2,13 @@
 
 helicopters = {
     1: ("Воздушный шар", 60, 0, "https://te.legra.ph/file/94ba572f4892d55b67400.jpg", 100000),
-    2: ("RotorWay Exec 162F", 185, 130, "https://te.legra.ph/file/ef8862d8ed65166bd729a.jpg", 3500000),
-    3: ("Robinson R44", 240, 260, "https://te.legra.ph/file/65893e7c188deae050ef1.jpg", 10000000),
-    4: ("Hiller UH-12C", 154, 300, "https://te.legra.ph/file/b26a1b234151bc964dcb2.jpg", 30000000),
-    5: ("AW119 Koala", 267, 420, "https://te.legra.ph/file/68145596e20a6dbf1742d.jpg", 63400000),
-    6: ("MBB BK 117", 262, 490, "https://te.legra.ph/file/4164edab2176c7ba5a77f.jpg", 150000000),
-    7: ("Eurocopter EC130", 287, 847, "https://te.legra.ph/file/2d0f6c3b6972ac5ad8e5c.jpg", 350000000),
-    8: ("Leonardo AW109 Power", 311, 550, "https://te.legra.ph/file/02f339e93e0d57fe553f9.jpg", 750000000),
-    9: ("Sikorsky S-76", 287, 420, "https://te.legra.ph/file/bcc31f92393c476d62c78.jpg", 1240000000),
-    10: ("Bell 429WLG", 273, 710, "https://te.legra.ph/file/ac4a0e331a18c88d4dec8.jpg", 8890000000),
-    11: ("NHI NH90", 300, 2100, "https://te.legra.ph/file/4f62df665e9fd63cb6696.jpg", 88330000000),
-    12: ("Kazan Mi-35M", 300, 1500, "https://te.legra.ph/file/8730dc435c142a22bc8f8.jpg", 225750000000),
-    13: ("Bell V-22 Osprey", 565, 6150, "https://te.legra.ph/file/5e31c429adc41d8e72c3e.jpg", 945300000000),
+   
 }
 
 
 phones = {
     1: ("Nokia 3310", "https://te.legra.ph/file/5f8aaca5fac3669d574ae.jpg", 100000),
-    2: ("ASUS ZenFone 4", "https://te.legra.ph/file/17a40669f482a3b297e15.jpg", 3500000),
-    3: ("BQ Aquaris X", "https://te.legra.ph/file/2162c4bf818ad37c8e928.jpg", 10000000),
-    4: ("Sony Xperia XA", "https://te.legra.ph/file/b2671d0b1892ed0c0ebf2.jpg", 30000000),
-    5: ("Samsung Galaxy S21 Ultra", "https://te.legra.ph/file/a8f194495a66626b8af90.jpg", 63400000),
-    6: ("Xiaomi Mi 11", "https://te.legra.ph/file/acb35e773b460e558ba17.jpg", 150000000),
-    7: ("iPhone 11 Pro Max", "https://te.legra.ph/file/c2da9f55f07671a73e213.jpg", 350000000),
-    8: ("iPhone 12 Pro Max", "https://te.legra.ph/file/f924d583f5d1ec7d6c788.jpg", 750000000),
-    9: ("Blackberry", "https://te.legra.ph/file/aa7ee3c1ab61c06407937.jpg", 1240000000),
+
 }
 
 
@@ -39,56 +21,17 @@ cars = {
 
 house = {
     1: ("Коробка", "https://te.legra.ph/file/926b8df6e95dc49540780.jpg", 500000),
-    2: ("Подвал", "https://te.legra.ph/file/1a13965b326eb85ebeb6f.jpg", 1000000),
-    3: ("Сарай", "https://te.legra.ph/file/a549dca565776bba55430.jpg", 3000000),
-    4: ("Маленький домик", "https://te.legra.ph/file/93145a842951e2fc6b35b.jpg", 5000000),
-    5: ("Квартира", "https://te.legra.ph/file/c02d6c0f2359b1ea091f3.jpg", 7000000),
-    6: ("Огромный дом", "https://te.legra.ph/file/bbb667ed2c915b2c04a8e.jpg", 10000000),
-    7: ("Коттедж", "https://te.legra.ph/file/ba24b5a15a191158a789e.jpg", 50000000),
-    8: ("Вилла", "https://te.legra.ph/file/e6652da23b2ea2356b6a8.jpg", 100000000),
-    9: ("Загородный дом", "https://te.legra.ph/file/387f53c114db249bb0bdc.jpg", 5000000000),
-    10: ("Небоскрёб", "https://te.legra.ph/file/8a383b9fa7a7f83bca73b.jpg", 50000000000),
-    11: ("Дом на мальдивах", "https://te.legra.ph/file/49db6245f86226db41dd9.jpg", 200000000000),
-    12: ("Технологичный небоскрёб", "https://te.legra.ph/file/d2593109100ca08edfb7d.jpg", 1000000000000),
-    13: ("Собственный остров", "https://te.legra.ph/file/a37cba3a27c35603ffadc.jpg", 5000000000000),
-    14: ("Дом на марсе", "https://te.legra.ph/file/96917531220a254a570b5.jpg", 15000000000000),
-    15: ("Остров на марсе", "https://te.legra.ph/file/9b66197b25d086f38b51e.jpg", 25000000000000),
-    16: ("Свой марс", "https://te.legra.ph/file/6e65b39089936c35aa759.jpg", 50000000000000),
+
 }
 
 
 yahts = {
     1: ("Ванна", 5, 100, "https://te.legra.ph/file/491c300689cbe160228ac.jpg", 1000000),
-    2: ("Nauticat 331", 11, 150, "https://te.legra.ph/file/c54cd589c8ca285d75bb4.jpg", 10000000),
-    3: ("Nordhavn 56 MS", 20, 165, "https://te.legra.ph/file/e87f9e0abe61c804b34ed.jpg", 30000000),
-    4: ("Princess V60", 38, 1200, "https://te.legra.ph/file/eb5b0e6f921ff68e28ad6.jpg", 100000000),
-    5: ("Bayliner 288", 40, 300, "https://te.legra.ph/file/3ca21f15fdace0ed40d21.jpg", 500000000),
-    6: ("Dominator 40M", 45, 2600, "https://te.legra.ph/file/8ca43fff773584a46ac63.jpg", 800000000),
-    7: ("Sessa Marine C42", 57, 400, "https://te.legra.ph/file/716284f2bb510e0512d3b.jpg", 5000000000),
-    8: ("Wider 150", 60, 720, "https://te.legra.ph/file/979369c9b754e63317d6c.jpg", 15000000000),
-    9: ("Palmer Johnson 42M SuperSport", 64, 3840, "https://te.legra.ph/file/17da4f0deb1e5ed728e5d.jpg", 40000000000),
-    10: ("Serene", 50, 33800, "https://te.legra.ph/file/4ecd422cedef17098a65a.jpg", 90000000000),
-    11: ("Dubai", 50, 33800, "https://te.legra.ph/file/9e948fcd44a8097824944.jpg", 200000000000),
-    12: ("Azzam", 62, 94000, "https://te.legra.ph/file/fb92f7f0955032c904ce4.jpg", 600000000000),
-    13: ("Streets of Monaco", 40, 9000, "https://te.legra.ph/file/0cd2b43532ce6460c92ac.jpg", 1600000000000),
+
 }
 
 
 planes = {
     1: ("Параплан", 20, 0, "https://te.legra.ph/file/f8de2aaf5e09aa0b8f429.jpg", 100000000),
-    2: ("АН-2", 258, 1000, "https://te.legra.ph/file/4110a533fb5bd23c8a5e5.jpg", 350000000),
-    3: ("Cessna-172E", 302, 145, "https://te.legra.ph/file/985ea5934cf5b0b94f970.jpg", 700000000),
-    4: ("BRM NG-5", 145, 100, "https://te.legra.ph/file/b0711291db0560217e515.jpg", 1000000000),
-    5: ("Cessna T210", 380, 320, "https://te.legra.ph/file/d24ec27763558536e2afe.jpg", 1400000000),
-    6: ("Beechcraft 1900D", 518, 1279, "https://te.legra.ph/file/ff14ac4901c3ffaf8f510.jpg", 2600000000),
-    7: ("Cessna 550", 746, 1500, "https://te.legra.ph/file/d2627d28debdf2646f5ac.jpg", 5500000000),
-    8: ("Hawker 4000", 1029, 1300, "https://te.legra.ph/file/f9c0caaa07c3d2db63c2d.jpg", 8800000000),
-    9: ("Learjet 31", 992, 1200, "https://te.legra.ph/file/b39a112c0abfe9f656c2a.jpg", 450000000000),
-    10: ("Airbus A318", 871, 2000, "https://te.legra.ph/file/2c92527d0d4c4e94eaa2b.jpg", 800000000000),
-    11: ("F-35A", 1400, 1450, "https://te.legra.ph/file/65103d128771b6d4bdb3e.jpg", 1600000000000),
-    12: ("Boeing 747-430", 896, 1600, "https://te.legra.ph/file/cc7efb643f7c743e9115e.jpg", 2250000000000),
-    13: ("C-17A Globemaster III", 950, 2040, "https://te.legra.ph/file/1b72e435f1d19e6747641.jpg", 3500000000000),
-    14: ("F-22 Raptor", 2414, 1800, "https://te.legra.ph/file/59ddb48f5e3a6a567136c.jpg", 4000000000000),
-    15: ("Airbus 380 Custom", 900, 4000, "https://te.legra.ph/file/698571677db7c6a02fee2.jpg", 6000000000000),
-    16: ("B-2 Spirit Stealth Bomber", 1010, 2400, "https://te.legra.ph/file/a4ecf2dc5f1b530b9f96a.jpg", 13500000000000),
+
 }
