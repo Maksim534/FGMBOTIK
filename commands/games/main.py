57a65712801e9f6284ee49853c27bf8d434ac3a2
Maksim534 <129356035+Maksim534@users.noreply.github.com>
2026-01-11 22:09:30 +0300

    Update main.py

--- commands/games/main.py
+++ commands/games/main.py
@@ -442,6 +442,6 @@ def reg(dp: Dispatcher):
     dp.message.register(casino_cmd, StartsWith("казино"))
     dp.message.register(spin_cmd, StartsWith("спин"))
     dp.message.register(trade_cmd, StartsWith("трейд вверх", "трейд вниз"))
-	dp.message.register(roulette_ruless, lambda message: message.text.lower() == "рулетка")
-	dp.message.register(roulette, lambda message: message.text.lower().startswith("рулетка"))
+	dp.message.register(roulette_ruless, StartsWith("рулетка"))
+	dp.message.register(roulette, StartsWith("рулетка"))
    
