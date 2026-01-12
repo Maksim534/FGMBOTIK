487a7c4b9de7a8e8f4e61ad303683096fc0f6f08
Maksim534 <129356035+Maksim534@users.noreply.github.com>
2026-01-12 18:19:19 +0300

    Update lists.py

--- commands/basic/property/lists.py
+++ commands/basic/property/lists.py
@@ -13,8 +13,30 @@ phones = {
 
 
 cars = {
-    1: ("Самокат", 25, 0, 0, "https://te.legra.ph/file/30c8327863e32f6167e25.jpg", 10000),
-    2: ("Peel P50", 64, 4, 0, "https://imgfoto.host/i/pHlUSu", 50000),
+    1: ("Самокат", 25, 0, 0, "https://te.legra.ph/file/30c8327863e32f6167e25.jpg", 10000),    https://imgfoto.host/i/p2uYy1
+    2: ("УАЗ Хантер", 125, 106, 32, "https://imgfoto.host/i/p2v0fw", 325000),
+    3: ("Peel P50", 64, 4, 0, "https://imgfoto.host/i/pHlUSu", 50000),
+    4: ("Daihatsu Terios kid", 139, 105, 13, "https://imgfoto.host/i/p2DmJE", 200000),
+    5: ("Mitsubishi Pajero Mini", 139, 64, 9.1, "https://imgfoto.host/i/p2D2ie", 370000),
+    6: ("Honda civic", 201, 178, 8.7, "https://imgfoto.host/i/p27lK1", 450000),
+    7: ("Acura Integra", 220, 235, 8, "https://imgfoto.host/i/p2Dg92", 650000),
+    8: ("Mazda MX-5 Miata", 179, 184, 9.1, "https://imgfoto.host/i/p2DXQI", 800000),
+    9: ("Opel Astra", 197, 120, 12, "https://imgfoto.host/i/p2zi7w", 1000000),
+    10: ("Audi 80", 185, 115, 11.8, "https://imgfoto.host/i/p2BCB5", 1200000),
+    11: ("Lada Granta", 197, 87, 13.1, "https://imgfoto.host/i/p2wxkx", 1500000),
+    12: ("Lincoln Continental", 217, 203, 8.1, "https://imgfoto.host/i/p27vl1", 2000000),
+    13: ("Volkswagen Golf GTI", 235, 227, 7.5, "https://imgfoto.host/i/p27PoR", 2500000),
+    14: ("Nissan Almera Classic", 184, 107, 12.1, "https://imgfoto.host/i/p2D2ie", 3100000),
+    15: ("BMW 3-series e36", 195, 195, 7.8, "https://imgfoto.host/i/p2BCB5", 4500000),
+    16: ("Mercedes-Benz w220", 245, 279, 7.8, "https://imgfoto.host/i/p27oTa", 6000000),
+    17: ("Ford Raptor", 273, 775, 4, "https://imgfoto.host/i/p2Kmsw", 8000000),
+    18: ("Dodge Durango", 245, 365, 6.4, "https://imgfoto.host/i/p2v1Wa", 16500000),
+    19: ("Infinity FX37", 233, 333, 6.8, "https://imgfoto.host/i/p2KKza", 40000000),
+    20: ("Porsche Cayenne S", 270, 440, 4.7, "https://imgfoto.host/i/p2DfTe", 80000000),
+    21: ("Jeep grand Cherokee", 215, 273, 8.2, "https://imgfoto.host/i/p2Kmsw", 300000000),
+    22: ("Aurus Senat", 255, 598, 6, "https://imgfoto.host/i/p2wxkx", 700000000),
+    23: ("Bugatti La Voiture Noire", 378, 1500, 2.4, "https://imgfoto.host/i/p2uYy1", 4000000000),
+    
     
 }
 
