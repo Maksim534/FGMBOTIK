2035887dd21d3a2883cac9ebf9f3e10edd33d635
Maksim534 <129356035+Maksim534@users.noreply.github.com>
2026-02-19 16:26:11 +0300

    Update custom.py

--- filters/custom.py
+++ filters/custom.py
@@ -1,4 +1,5 @@
 from typing import Union
+import config as cfg
 
 from aiogram.filters import BaseFilter
 from aiogram.types import Message, CallbackQuery
@@ -19,19 +20,26 @@ class TextIn(BaseFilter):
         return text and text.lower() in self.values
 
 
+
 class StartsWith(BaseFilter):
-    def __init__(self, *prefixes: str) -> None:
+    def __init__(self, *prefixes: str):
         self.prefixes = [p.lower() for p in prefixes]
 
-    async def __call__(self, obj: Union[Message, CallbackQuery]) -> bool:
-        text = None
-
-        if isinstance(obj, Message):
-            text = obj.text
-        elif isinstance(obj, CallbackQuery):
-            text = obj.data
-
-        if not text:
+    async def __call__(self, message: Message) -> bool:
+        if not message.text:
             return False
-
-        return any(text.lower().startswith(prefix) for prefix in self.prefixes)
+        
+        text = message.text.lower()
+        bot_username = f"@{cfg.bot_username.lower()}"
+        
+        # Если сообщение начинается с @бота, убираем его
+        if text.startswith(bot_username):
+            text = text[len(bot_username):].strip()
+            # Обновляем текст сообщения для дальнейшей обработки
+            message.text = message.text[len(bot_username):].strip()
+        
+        # Проверяем все префиксы
+        for prefix in self.prefixes:
+            if text.startswith(prefix):
+                return True
+        return False
