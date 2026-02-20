928144c2c50cb7b1e8d9ea382426449dc23e5c7c
Maksim534 <129356035+Maksim534@users.noreply.github.com>
2026-02-20 11:35:27 +0300

    Update main.py

--- commands/basic/property/main.py
+++ commands/basic/property/main.py
@@ -4,6 +4,7 @@ from commands.basic.property.autosalon import reg as autosalon_reg
 from commands.basic.property.helicopter_salon import reg as heli_reg
 from commands.basic.property.house_salon import reg as house_salon_reg
 from commands.basic.property.phone_salon import reg as phone_salon_reg
+from commands.basic.property.plane_salon import reg as plane_salon_reg
 
 import commands.basic.property.db as db
 from assets.antispam import antispam
@@ -710,6 +711,7 @@ def reg(dp: Dispatcher):
     autosalon_reg(dp)
     house_salon_reg(dp)
     phone_salon_reg(dp)
+    plane_salon_reg(dp)
     # Колбэки для автомобиля
     dp.message.register(helicopters_list, TextIn("вертолеты", "вертолёты"))
     dp.message.register(cars_list, TextIn("машины"))
