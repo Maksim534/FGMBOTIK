20b98ac999e5a06ef3f9a1a81a3f76bb6074d3a5
Maksim534 <129356035+Maksim534@users.noreply.github.com>
2026-02-21 00:38:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -32,6 +32,7 @@ MODULES = [
     'commands.games.main',
     'commands.games.miracles',
     'commands.games.refferals',
+    'commands.games.tictactoe',
     'commands.basic.bank.main',
     'commands.entertaining.case.main',
     'commands.entertaining.earnings.garden.potions',
