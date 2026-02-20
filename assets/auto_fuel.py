b366b46d9a48a8e0b7c6afe7f4a78c165b7a0fac
Maksim534 <129356035+Maksim534@users.noreply.github.com>
2026-02-20 05:45:27 +0300

    Update auto_fuel.py

--- assets/auto_fuel.py
+++ assets/auto_fuel.py
@@ -5,12 +5,8 @@ async def auto_fuel():
     """Уменьшение топлива у всех машин каждые 10 минут"""
     while True:
         try:
-            cursor.execute("UPDATE property SET fuel = max(0, fuel - 20) WHERE car > 0 AND fuel > 0")
-            conn.commit()
-            print("⛽ Топливо уменьшено у всех машин")
-        except Exception as e:
-            print(f"Ошибка при уменьшении топлива: {e}")
-        await asyncio.sleep(600)  # 10 минут
+            cursor.execute("UPDATE property SET fuel = max(0, fuel - 1) WHERE car > 0 AND fuel > 0")
+            conn.commit()# 10 минут
 
 # Запуск в основном файле
 async def start_auto_fuel():
